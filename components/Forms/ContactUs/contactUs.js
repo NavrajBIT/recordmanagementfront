1929a6344ceb33e2810a9da1e5faa0d83dd3ca54
inovatormatin <manojgeetparmar@gmail.com>
2022-06-01 10:08:13 +0530

    Adding new code in contact us

--- components/Forms/ContactUs/contactUs.js
+++ components/Forms/ContactUs/contactUs.js
@@ -1,29 +1,26 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "../../../styles/contactUs.module.css";
-
+// import Navbar from "./Navbar";
 const ContactUs = () => {
   const [info, setinfo] = useState({
     heading: "",
     description: "",
   });
-
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-
     setinfo({ ...info, [name]: value });
   };
-
-
   return (
     <>
       <div className={styles.contactUs}>
+        {/* <div className="navbar">
+          <Navbar/>
+        </div> */}
         <div className={styles.heading}>
           <h2>Contact Us</h2>
         </div>
-
         <div className={styles.contactInfo}>
-          
             <div className={styles.headingBlock}>
               <label htmlFor="heading">Heading</label>
               <input
@@ -33,9 +30,9 @@ const ContactUs = () => {
                 placeholder="Add Heading"
                 value={info.heading}
                 onChange={handleChange}
+                autoComplete='off'
               />
             </div>
-
             <div className={styles.descriptionBlock}>
               <label className={styles.descLabel} htmlFor="description">
                 Description
@@ -50,19 +47,15 @@ const ContactUs = () => {
                 onChange={handleChange}
               ></textarea>
             </div>
-
             <div className={styles.browseButton}>
               <button>Browse</button>
             </div>
-
             <div className={styles.submitButton}>
-              <button >Submit</button>
+              <button>Submit</button>
             </div>
-           
         </div>
       </div>
     </>
   );
 };
-
 export default ContactUs;
